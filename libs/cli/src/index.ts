e1da9dc75abc2cb8552e4f40f2ae86b787c796ad
EndyKaufman <admin@site15.ru>
2024-01-08 22:18:25 +0400

    fix(cli): update code

--- libs/cli/src/index.ts
+++ libs/cli/src/index.ts
@@ -1,1 +1,5 @@
-throw new Error('There is no functionality now, this name is reserved for future command utilities of the NestJS-mod project.')
+function main() {
+    throw new Error('There is no functionality now, this name is reserved for future command utilities of the NestJS-mod project.')
+}
+
+main();
