79c02ff29408fd00333becc61efaa140a134e7f9
EndyKaufman <admin@site15.ru>
2024-01-08 22:02:42 +0400

    chore: reserve console tools name

---
+++ libs/cli/README.md
@@ -0,0 +1,1 @@
+There is no functionality now, this name is reserved for future command utilities of the NestJS-mod project.
---
+++ libs/cli/src/index.ts
@@ -0,0 +1,1 @@
+throw new Error('There is no functionality now, this name is reserved for future command utilities of the NestJS-mod project.')
