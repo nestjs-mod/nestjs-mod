cbdf10ad137ed47a9f3a65680bc3967439d2fb3d
EndyKaufman <admin@site15.ru>
2024-01-11 12:53:39 +0400

    chore: remove close service [ci skip]

--- apps/cli/src/close.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
-
-@Injectable()
-export class CloseService implements OnApplicationBootstrap {
-  onApplicationBootstrap() {
-    process.exit();
-  }
-}
