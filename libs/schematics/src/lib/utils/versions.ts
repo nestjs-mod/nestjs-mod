ba674f1b58df6659b7aa3d2d2489a70f59e86990
EndyKaufman <admin@site15.ru>
2024-01-20 21:39:44 +0400

    chore: update schematics [skip integrations]

--- libs/schematics/src/lib/utils/versions.ts
+++ libs/schematics/src/lib/utils/versions.ts
@@ -13,5 +13,5 @@ export const nestJsModDeps = {
 };
 export const nestJsModDevDeps = {
   '@nestjs-mod/testing': '2.2.0',
-  '@nestjs-mod/schematics': '2.0.1'
+  '@nestjs-mod/schematics': '2.0.2'
 };
