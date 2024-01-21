7a0c15dd443f307e047673a0014e107063000102
EndyKaufman <admin@site15.ru>
2024-01-21 13:29:00 +0400

    chore: update for nestjs-mod v.2.3.1 [skip ci]

--- libs/schematics/src/lib/utils/versions.ts
+++ libs/schematics/src/lib/utils/versions.ts
@@ -13,5 +13,5 @@ export const nestJsModDeps = {
 };
 export const nestJsModDevDeps = {
   '@nestjs-mod/testing': '2.3.1',
-  '@nestjs-mod/schematics': '2.0.2'
+  '@nestjs-mod/schematics': '2.0.3'
 };
