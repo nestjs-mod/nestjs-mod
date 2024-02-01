1ff601d04916bc814b78e37d418c13dc9c18541b
EndyKaufman <admin@site15.ru>
2024-02-01 19:11:57 +0400

    fix: update versions of contrib modules [skip integrations, skip contrib]

--- libs/schematics/src/lib/generators/init/lib/add-dependencies.ts
+++ libs/schematics/src/lib/generators/init/lib/add-dependencies.ts
@@ -3,7 +3,7 @@ import { addDependenciesToPackageJson } from '@nx/devkit';
 import { nestJsModDeps, nestJsModDevDeps, nestJsSchematicsVersion } from '../../../utils/versions';
 
 export function addDependencies(tree: Tree): GeneratorCallback {
-  const nestJsModSchematicsVersion = '2.2.5';
+  const nestJsModSchematicsVersion = '2.2.6';
   return addDependenciesToPackageJson(
     tree,
     {
--- libs/schematics/src/lib/utils/versions.ts
+++ libs/schematics/src/lib/utils/versions.ts
@@ -7,11 +7,11 @@ export const tsLibVersion = '^2.3.0';
 export const nestJsModDeps = {
   '@nestjs-mod/common': '2.6.1',
   '@nestjs-mod/reports': '2.6.1',
-  '@nestjs-mod/terminus': '1.6.1',
-  '@nestjs-mod/pino': '1.7.1',
-  '@nestjs-mod/pm2': '1.5.1'
+  '@nestjs-mod/terminus': '1.7.0',
+  '@nestjs-mod/pino': '1.8.0',
+  '@nestjs-mod/pm2': '1.6.1'
 };
 export const nestJsModDevDeps = {
   '@nestjs-mod/testing': '2.6.1',
-  '@nestjs-mod/schematics': '2.2.4'
+  '@nestjs-mod/schematics': '2.2.5'
 };
