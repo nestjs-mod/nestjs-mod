2ae576f4ce7aead6aa12f0fba9e1b017c729e1a7
EndyKaufman <admin@site15.ru>
2024-02-08 13:52:29 +0400

    chore: update versions

--- libs/schematics/src/lib/utils/versions.ts
+++ libs/schematics/src/lib/utils/versions.ts
@@ -5,13 +5,13 @@ export const reflectMetadataVersion = '^0.1.13';
 export const tsLibVersion = '^2.3.0';
 
 export const nestJsModDeps = {
-  '@nestjs-mod/common': '2.7.0',
-  '@nestjs-mod/reports': '2.7.0',
+  '@nestjs-mod/common': '2.8.0',
+  '@nestjs-mod/reports': '2.8.0',
   '@nestjs-mod/terminus': '1.8.0',
   '@nestjs-mod/pino': '1.9.0',
   '@nestjs-mod/pm2': '1.7.0'
 };
 export const nestJsModDevDeps = {
-  '@nestjs-mod/testing': '2.7.0',
-  '@nestjs-mod/schematics': '2.3.2'
+  '@nestjs-mod/testing': '2.8.0',
+  '@nestjs-mod/schematics': '2.4.0'
 };
