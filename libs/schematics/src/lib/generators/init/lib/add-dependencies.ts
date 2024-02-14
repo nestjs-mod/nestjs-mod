79b3f5f7290b24db7645bd1db989452d382befb7
EndyKaufman <admin@site15.ru>
2024-02-14 13:31:39 +0400

    chore: update libs versions [skip integrations, skip contrib]

--- libs/schematics/src/lib/generators/init/lib/add-dependencies.ts
+++ libs/schematics/src/lib/generators/init/lib/add-dependencies.ts
@@ -3,7 +3,7 @@ import { addDependenciesToPackageJson } from '@nx/devkit';
 import { nestJsModDeps, nestJsModDevDeps, nestJsSchematicsVersion } from '../../../utils/versions';
 
 export function addDependencies(tree: Tree): GeneratorCallback {
-  const nestJsModSchematicsVersion = '2.5.3';
+  const nestJsModSchematicsVersion = '2.6.1';
   return addDependenciesToPackageJson(
     tree,
     {
