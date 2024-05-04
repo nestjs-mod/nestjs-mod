a0c35e0700a962fb77fcf60bc140587197612114
EndyKaufman <admin@site15.ru>
2024-05-04 13:24:34 +0400

    fix: update libs @nestjs-mod/common@2.13.4 versions [skip integrations, skip contrib]

--- libs/schematics/src/lib/utils/versions.ts
+++ libs/schematics/src/lib/utils/versions.ts
@@ -5,13 +5,13 @@ export const reflectMetadataVersion = '^0.1.13';
 export const tsLibVersion = '^2.3.0';
 
 export const nestJsModDeps = {
-  '@nestjs-mod/common': '2.13.3',
-  '@nestjs-mod/reports': '2.13.3',
+  '@nestjs-mod/common': '2.13.4',
+  '@nestjs-mod/reports': '2.13.4',
   '@nestjs-mod/terminus': '1.12.0',
   '@nestjs-mod/pino': '1.13.0',
   '@nestjs-mod/pm2': '1.11.0'
 };
 export const nestJsModDevDeps = {
-  '@nestjs-mod/testing': '2.13.3',
-  '@nestjs-mod/schematics': '2.8.2'
+  '@nestjs-mod/testing': '2.13.4',
+  '@nestjs-mod/schematics': '2.8.3'
 };
