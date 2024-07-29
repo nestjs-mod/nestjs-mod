1fb00e82afb9c27f6557b7a5380e60c20a90fb54
EndyKaufman <admin@site15.ru>
2024-07-29 10:00:06 +0400

    fix: update libs @nestjs-mod/common@2.14.0 versions [skip integrations, skip contrib]

--- libs/schematics/src/lib/generators/init/lib/add-dependencies.ts
+++ libs/schematics/src/lib/generators/init/lib/add-dependencies.ts
@@ -3,17 +3,17 @@ import { addDependenciesToPackageJson } from '@nx/devkit';
 import { nestJsModDeps, nestJsModDevDeps, nestJsSchematicsVersion } from '../../../utils/versions';
 
 export function addDependencies(tree: Tree): GeneratorCallback {
-  const nestJsModSchematicsVersion = '2.8.4';
+  const nestJsModSchematicsVersion = '2.9.1';
   return addDependenciesToPackageJson(
     tree,
     {
       ...nestJsModDeps,
-      '@nestjs/terminus': '^10.2.0',
+      '@nestjs/terminus': '^10.2.3',
       'case-anything': '^2.1.13',
       'class-validator': '^0.14.0',
-      'nestjs-pino': '^4.0.0',
-      'pino-http': '^9.0.0',
-      'pino-pretty': '^10.3.1',
+      'nestjs-pino': '^4.1.0',
+      'pino-http': '^10.2.0',
+      'pino-pretty': '^11.2.2',
       pm2: '^5.3.0',
       dotenv: '>=16.3.1',
     },
@@ -22,9 +22,9 @@ export function addDependencies(tree: Tree): GeneratorCallback {
       '@nestjs-mod/schematics': nestJsModSchematicsVersion,
       '@commitlint/cli': '^17.0.0',
       '@commitlint/config-conventional': '^17.0.0',
-      '@theunderscorer/nx-semantic-release': '^2.10.0',
+      '@theunderscorer/nx-semantic-release': '^2.11.0',
       '@nestjs/schematics': nestJsSchematicsVersion,
-      rucken: '^4.6.4',
+      rucken: '^4.8.1',
       prettier: '^2.6.2',
       'lint-staged': '^15.2.0',
     }
