de25c00bb8107c13042e086e3562b4d15c180b23
EndyKaufman <admin@site15.ru>
2024-08-05 13:30:49 +0400

    chore: update link to license file [ci skip]

--- libs/schematics/src/lib/utils/versions.ts
+++ libs/schematics/src/lib/utils/versions.ts
@@ -5,13 +5,13 @@ export const reflectMetadataVersion = '^0.1.13';
 export const tsLibVersion = '^2.3.0';
 
 export const nestJsModDeps = {
-  '@nestjs-mod/common': '2.14.0',
-  '@nestjs-mod/reports': '2.14.0',
+  '@nestjs-mod/common': '2.14.1',
+  '@nestjs-mod/reports': '2.14.1',
   '@nestjs-mod/terminus': '1.13.0',
   '@nestjs-mod/pino': '1.14.0',
   '@nestjs-mod/pm2': '1.12.0'
 };
 export const nestJsModDevDeps = {
-  '@nestjs-mod/testing': '2.14.0',
-  '@nestjs-mod/schematics': '2.9.1'
+  '@nestjs-mod/testing': '2.14.1',
+  '@nestjs-mod/schematics': '2.9.2'
 };
