9d5e89bbb6f60e1e065e96bccc1df91d924c1d12
EndyKaufman <admin@site15.ru>
2024-11-03 13:56:06 +0400

    fix: update version of schematic library [skip integrations, skip contrib]

--- libs/schematics/src/lib/generators/init/lib/add-dependencies.ts
+++ libs/schematics/src/lib/generators/init/lib/add-dependencies.ts
@@ -3,7 +3,7 @@ import { addDependenciesToPackageJson } from '@nx/devkit';
 import { nestJsModDeps, nestJsModDevDeps, nestJsSchematicsVersion } from '../../../utils/versions';
 
 export function addDependencies(tree: Tree): GeneratorCallback {
-  const nestJsModSchematicsVersion = '2.11.1';
+  const nestJsModSchematicsVersion = '2.11.2';
   return addDependenciesToPackageJson(
     tree,
     {
--- libs/schematics/src/lib/utils/versions.ts
+++ libs/schematics/src/lib/utils/versions.ts
@@ -5,13 +5,13 @@ export const reflectMetadataVersion = '^0.1.13';
 export const tsLibVersion = '^2.3.0';
 
 export const nestJsModDeps = {
-  '@nestjs-mod/common': '2.15.0',
-  '@nestjs-mod/reports': '2.15.0',
-  '@nestjs-mod/terminus': '1.13.2',
-  '@nestjs-mod/pino': '1.14.2',
-  '@nestjs-mod/pm2': '1.12.2'
+  '@nestjs-mod/common': '2.16.3',
+  '@nestjs-mod/reports': '2.16.3',
+  '@nestjs-mod/terminus': '1.13.3',
+  '@nestjs-mod/pino': '1.14.3',
+  '@nestjs-mod/pm2': '1.12.3'
 };
 export const nestJsModDevDeps = {
-  '@nestjs-mod/testing': '2.15.0',
-  '@nestjs-mod/schematics': '2.11.1'
+  '@nestjs-mod/testing': '2.16.3',
+  '@nestjs-mod/schematics': '2.11.2'
 };
