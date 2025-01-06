30333eb21a491cb66996af11b3740aa25675b2da
EndyKaufman <admin@site15.ru>
2025-01-06 18:41:50 +0400

    fix: rename nx library from cli to nestjs-mod

--- libs/nestjs-mod/jest.config.ts
+++ libs/nestjs-mod/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/cli',
+  coverageDirectory: '../../coverage/libs/nestjs-mod',
 };
