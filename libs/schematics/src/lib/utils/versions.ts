c3c444e2fe85f333665bdf5d7c5d934fee27aa8a
EndyKaufman <admin@site15.ru>
2025-01-22 15:52:37 +0400

    fix: lock version of case-anything

--- libs/schematics/src/lib/utils/versions.ts
+++ libs/schematics/src/lib/utils/versions.ts
@@ -1,17 +1,17 @@
-export const nestJsVersion = '^10.4.15';
-export const nestJsSchematicsVersion = '^10.2.3';
+export const nestJsVersion = '^10.0.2';
+export const nestJsSchematicsVersion = '^10.0.1';
 export const rxjsVersion = '^7.8.0';
 export const reflectMetadataVersion = '^0.1.13';
 export const tsLibVersion = '^2.3.0';
 
 export const nestJsModDeps = {
-  '@nestjs-mod/common': '2.17.0',
-  '@nestjs-mod/reports': '2.17.0',
-  '@nestjs-mod/terminus': '1.13.4',
-  '@nestjs-mod/pino': '1.14.4',
-  '@nestjs-mod/pm2': '1.12.4'
+  '@nestjs-mod/common': '2.17.3',
+  '@nestjs-mod/reports': '2.17.3',
+  '@nestjs-mod/terminus': '1.14.0',
+  '@nestjs-mod/pino': '1.15.0',
+  '@nestjs-mod/pm2': '1.13.0'
 };
 export const nestJsModDevDeps = {
-  '@nestjs-mod/testing': '2.17.0',
-  '@nestjs-mod/schematics': '2.12.0'
+  '@nestjs-mod/testing': '2.17.3',
+  '@nestjs-mod/schematics': '2.12.3'
 };
