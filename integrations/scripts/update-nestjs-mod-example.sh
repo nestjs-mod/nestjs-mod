26c524bae514018abd4516d68c2f70ea58dfb611
EndyKaufman <admin@site15.ru>
2025-01-27 15:34:30 +0400

    chore: update rucken version [skip contrib, skip integrations]

--- integrations/scripts/update-nestjs-mod-example.sh
+++ integrations/scripts/update-nestjs-mod-example.sh
@@ -8,7 +8,10 @@ export NX_DAEMON=false
 rm -rf ./dist
 rm -rf ./integrations/tmp
 mkdir -p ./integrations/tmp
+rm -rf ./integrations/nestjs-mod-example
+git clone git@github.com:nestjs-mod/nestjs-mod-example.git ./integrations/nestjs-mod-example
 cd ./integrations/nestjs-mod-example
+rm -r ./*
 rm -rf ./node_modules
 rm -rf ./apps
 rm -rf ./libs
@@ -26,9 +29,9 @@ yes | cp -rf ./integrations/tmp/project-name/* ./integrations/nestjs-mod-example
 cd ./integrations/nestjs-mod-example
 npm install --save-dev @nestjs-mod/schematics@latest
 rm -rf ./.nx
-./node_modules/.bin/nx g @nestjs-mod/schematics:application --linter=eslint --unitTestRunner=jest --directory=apps/app-name --name=app-name --strict=true
+./node_modules/.bin/nx g @nestjs-mod/schematics:application --verbose --linter=eslint --unitTestRunner=jest --directory=apps/app-name --name=app-name --strict=true
 rm -rf ./.nx
-./node_modules/.bin/nx g @nestjs-mod/schematics:library --linter=eslint --unitTestRunner=jest --buildable --publishable --directory=libs/feature-name --simpleName=true --strict=true
+./node_modules/.bin/nx g @nestjs-mod/schematics:library --verbose --linter=eslint --unitTestRunner=jest --buildable --publishable --directory=libs/feature-name --simpleName=true --strict=true
 rm -rf ./.nx
 npm run manual:prepare
 # todo: fix it later
