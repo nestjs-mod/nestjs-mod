0fa95a2382b1e470172def41db01c671b341fd26
EndyKaufman <admin@site15.ru>
2025-04-28 12:13:47 +0400

    fix: update nestjs-mod deps [skip contrib, skip integrations]

--- libs/schematics/src/lib/utils/versions.ts
+++ libs/schematics/src/lib/utils/versions.ts
@@ -5,14 +5,14 @@ export const reflectMetadataVersion = '^0.1.13';
 export const tsLibVersion = '^2.3.0';
 
 export const nestJsModDeps = {
-  '@nestjs-mod/common': '2.17.6',
-  '@nestjs-mod/reports': '2.17.6',
+  '@nestjs-mod/common': '2.17.7',
+  '@nestjs-mod/reports': '2.17.7',
   '@nestjs-mod/terminus': '1.14.2',
   '@nestjs-mod/pino': '1.15.2',
   '@nestjs-mod/pm2': '1.13.3'
 };
 export const nestJsModDevDeps = {
   'rucken': '^4.16.0',
-  '@nestjs-mod/testing': '2.17.6',
-  '@nestjs-mod/schematics': '2.12.6'
+  '@nestjs-mod/testing': '2.17.7',
+  '@nestjs-mod/schematics': '2.12.7'
 };
